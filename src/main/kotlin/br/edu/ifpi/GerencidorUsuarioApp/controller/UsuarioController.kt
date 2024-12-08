7c004b4fcec6f79f25aa8d1ba766415aeccc2b26
kitoumasaomi <kanfutrooper@gmail.com>
2024-12-08 16:42:23 +0900

    controller/UsuarioController.ktを作成する

--- src/main/kotlin/br/edu/ifpi/GerencidorUsuarioApp/controller/UsuarioController.kt
+++ src/main/kotlin/br/edu/ifpi/GerencidorUsuarioApp/controller/UsuarioController.kt
@@ -2,4 +2,11 @@ package br.edu.ifpi.GerencidorUsuarioApp.controller
 import org.springframework.stereotype.Controller
 
 @Controller
-class UsuarioController {}
+class UsuarioController {
+
+    @GetMapping("/formulario/cadastro")
+    fun abrirFormularioCadastro(): String {
+
+        return "formulario-cadastro"   
+    }
+}
